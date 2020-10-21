c640b8d1abe19cca1fd56c6cb48b553f941f6cb3
Jizong Liang <Liang_Jizong@comcast.com>
2020-10-20 23:16:05 -0400

    added alice middle package

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,13 +1,19 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/justinas/alice" // New import
+)
 
 func (app *application) routes() http.Handler {
+	standardMiddleware := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", app.home)
 	mux.HandleFunc("/snippet", app.showSnippet)
 	mux.HandleFunc("/snippet/create", app.createSnippet)
 	fileServer := http.FileServer(http.Dir("./ui/static"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	return app.recoverPanic(app.logRequest(secureHeaders(mux)))
+	return standardMiddleware.Then(mux)
+	// return app.recoverPanic(app.logRequest(secureHeaders(mux)))
 }
