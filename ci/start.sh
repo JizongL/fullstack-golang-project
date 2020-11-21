75ef978ae29f702aa816588ac7ade0e736e93629
JizongL <2015rpro@gmail.com>
2020-11-20 20:43:34 -0500

    updated nginx and start.sh

--- ci/start.sh
+++ ci/start.sh
@@ -1,6 +1,8 @@
 sudo yum update -y
 sudo yum install -y golang
 sudo yum install -y mysql
-sudo amazon-linux-extras install nginx1s -y
+sudo amazon-linux-extras install nginx -y
+sudo cp golang-app/nginx/golang-app.conf /etc/nginx/conf.d/golang-app.conf
+
 
 
