915c680ff633c38f47e94f90b76a897069a3e441
JizongL <2015rpro@gmail.com>
2020-11-20 20:54:05 -0500

    updated start.sh

--- ci/start.sh
+++ ci/start.sh
@@ -3,6 +3,10 @@ sudo yum install -y golang
 sudo yum install -y mysql
 sudo amazon-linux-extras install nginx -y
 sudo cp golang-app/nginx/golang-app.conf /etc/nginx/conf.d/golang-app.conf
-
-
+cd /tmp
+wget -O epel.rpm –nv https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+sudo yum install -y ./epel.rpm
+sudo yum install python2-certbot.noarch
+sudo yum install python-certbot-nginx
+sudo certbot --nginx
 
